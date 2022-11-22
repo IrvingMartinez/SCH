357369d4c0165e9438b6d1097973aa62311903fe
imartinez93 <irvingmar93@gmail.com>
2022-11-22 15:58:44 -0500

    desing entry employee

--- core/models/Pages_model.php
+++ core/models/Pages_model.php
@@ -8,20 +8,4 @@ class Pages_model extends Model
 		parent::__construct();
 	}
 
-	public function get_user_info()
-	{
-
-		// if( is_null($data) )
-		// return false;
-
-		return $this->database->select(
-			'entries',
-			[
-				'check_time'
-			]
-		);
-
-		// return (isset($query[0]) && !empty($query[0]) ) ? $query[0] : null;
-	}
-
 }
